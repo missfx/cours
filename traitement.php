a22331a3d81d89a662e00ed4f68d8f7880f8262a
Steph <stephanie.vachon@velvetcocoon.com>
2019-02-07 14:12:19 +0100

    Modifs et ajout test

--- traitement.php
+++ traitement.php
@@ -1,63 +1,63 @@
 <?php
 if(isset($_POST['email'])) {
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    // Editer les 2 lines suivantes pour l'envoi du mail
+    $email_to = "stephanie.vachon@velvetcocoon.com";
+    $email_subject = "Votre objet de mail";
  
     function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        // en cas d'erreur :
+        echo "Nous sommes désolés mais des erreurs ont été trouvées lors de l'envoi du formulaire. ";
+        echo "Ces erreurs apparaissent ci-dessous<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "Merci de les corriger<br /><br />";
         die();
     }
  
  
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
+    // validation des données existantes
+    if(!isset($_POST['prenom']) ||
+        !isset($_POST['nom']) ||
         !isset($_POST['email']) ||
         !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        !isset($_POST['commentaires'])) {
+        died('Nous sommes désolés mais il y a des erreurs');       
     }
  
      
  
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
+    $nom = $_POST['prenom']; // requis
+    $prenom = $_POST['nom']; // requis
+    $email_from = $_POST['email']; // requis
+    $telephone = $_POST['telephone']; // non requis
+    $commentaires = $_POST['commentaires']; // requis
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    $error_message .= 'Email non valide<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  if(!preg_match($string_exp,$prenom)) {
+    $error_message .= 'Prénom non valide<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+  if(!preg_match($string_exp,$nom)) {
+    $error_message .= 'Nom non valide<br />';
   }
  
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  if(strlen($commentaires) < 2) {
+    $error_message .= 'Commentaires non valide<br />';
   }
  
   if(strlen($error_message) > 0) {
     died($error_message);
   }
  
-    $email_message = "Form details below.\n\n";
+    $email_message = "Détails du formulaire \n\n";
  
      
     function clean_string($string) {
@@ -67,22 +67,22 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Prenom : ".clean_string($prenom)."\n";
+    $email_message .= "Nom: ".clean_string($nom)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Commentaires: ".clean_string($commentaires)."\n";
  
-// create email headers
+// création du header de l'email
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
-<!-- include your own success html here -->
+<!-- Inclure du html ici -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+Merci de nous avoir contacté. Nous vous répondrons bientôt.
  
 <?php
  
