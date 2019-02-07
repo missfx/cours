c6dec795eda897372f8a9d978f0fdf7dea49f8e1
Steph <stephanie.vachon@velvetcocoon.com>
2019-02-07 17:23:25 +0100

    modif

--- form.php
+++ form.php
@@ -1,11 +1,12 @@
 <form name="contactform" method="post" action="traitement.php">
+    <h1>titre formulaire</h1>
 <table width="450px">
 <tr>
  <td valign="top">
   <label for="prenom">Prénom *</label>
  </td>
  <td valign="top">
-  <input  type="text" name="prenom" maxlength="50" size="30">
+  <input type="text" name="prenom" maxlength="50" size="30">
  </td>
 </tr>
 <tr>
@@ -13,7 +14,7 @@
   <label for="nom">Nom *</label>
  </td>
  <td valign="top">
-  <input  type="text" name="nom" maxlength="50" size="30">
+  <input type="text" name="nom" maxlength="50" size="30">
  </td>
 </tr>
 <tr>
@@ -21,7 +22,7 @@
   <label for="email">Email *</label>
  </td>
  <td valign="top">
-  <input  type="text" name="email" maxlength="80" size="30">
+  <input type="text" name="email" maxlength="80" size="30">
  </td>
 </tr>
 <tr>
@@ -37,12 +38,12 @@
   <label for="commentaires">Commentaires *</label>
  </td>
  <td valign="top">
-  <textarea  name="commentaires" maxlength="1000" cols="25" rows="6"></textarea>
+  <textarea name="commentaires" maxlength="1000" cols="25" rows="6"></textarea>
  </td>
 </tr>
 <tr>
  <td colspan="2" style="text-align:center">
-  <input type="submit" value="Submit">   <a href="http://form.velvetcocoon.com/traitement">Envoyer</a>
+  <input type="submit" value="Submit">   <a href="http://form.velvetcocoon.com/traitement.php">Envoyer</a>
  </td>
 </tr>
 </table>
